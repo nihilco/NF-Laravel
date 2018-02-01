1326011ce651459fb2c4ea15ba1291d32d385770
Ubuntu <ubuntu@iris.nihil.co>
2018-02-01 15:31:01 +0000

    Created under construction page.

--- resources/views/default/index.blade.php
+++ resources/views/default/index.blade.php
@@ -1,11 +1,13 @@
-@extends('layouts.main')
+@extends('layouts.construction')
 
 @section('title', 'Home')
 
 @section('content')
-            <div class="content">
-                <div class="title m-b-md">
-                    <?= $app->config['view']['title'] ?>
-                </div>
-            </div>
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Under Construction</h1>
+	<p class="lead">This website will be up and running soon.</p>
+	<div class="progress mb-3 active">
+	  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%"></div>
+	</div>
+      </main>
 @endsection
