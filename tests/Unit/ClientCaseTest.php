c994c4f977c6399db7418ea4be7bc57319cafd57
Uriah M. Clemmer IV <uriah@nihil.co>
2018-02-02 20:48:29 +0000

    Created Client Management tests.

---
+++ tests/Unit/ClientCaseTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ClientCaseTest extends TestCase
+{
+    public $clientCase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+	$this->clientCase = factory(\App\Models\ClientCase::class)->create();
+    }
+
+    public function test_a_case_has_owner()
+    {
+        $this->assertInstanceOf(\App\Models\User::class, $this->clientCase->owner);
+    }
+
+    public function test_a_case_has_creator()
+    {
+        $this->assertInstanceOf(\App\Models\User::class, $this->clientCase->creator);
+    }
+
+    public function test_a_case_has_client()
+    {
+        $this->assertInstanceOf(\App\Models\Client::class, $this->clientCase->client);
+    }
+}
