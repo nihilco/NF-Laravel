3dd8d6c90629d16a99e777607bd2f33b3586719b
Uriah M. Clemmer IV <uriah@nihil.co>
2018-04-11 12:18:49 -0400

    Fixed database merge.

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $this->call([
+            UsersTableSeeder::class,
+            CountriesTableSeeder::class,
+            ProvincesTableSeeder::class,
+            AccountsTableSeeder::class,
+            CustomersTableSeeder::class,
+            DomainsTableSeeder::class,
+            WebsitesTableSeeder::class,
+            AliasesTableSeeder::class,
+            InvoicesTableSeeder::class,
+            PagesTableSeeder::class,
+            PostsTableSeeder::class,
+            CommentsTableSeeder::class,
+            TutorialsTableSeeder::class,
+            ChannelsTableSeeder::class,
+            ForumsTableSeeder::class,
+            ThreadsTableSeeder::class,
+            AuthorsTableSeeder::class,
+            SeriesTableSeeder::class,
+            BooksTableSeeder::class,
+            ClientsTableSeeder::class,
+            CasesTableSeeder::class,
+            CaseNotesTableSeeder::class,
+            CaseTypesTableSeeder::class,
+            LotteriesTableSeeder::class,
+        ]);
+    }
+}
